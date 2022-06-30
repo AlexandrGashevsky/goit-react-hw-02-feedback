359433fbc061792853ad4c28ce5189b23075b1d1
AlexandrGashevsky <71451324+AlexandrGashevsky@users.noreply.github.com>
2022-07-01 00:24:21 +0300

    2

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import Section from "./components/Section/Section";
 import FeedbackOptions from "./components/FeedbackOptions/FeedbackOptions";
 import Statistic from "./components/Statistic/Statistic";
 import Notification from "./components/Notification/Notification";
+
 class App extends React.Component {
   state = {
     good: 0,
     neutral: 0,
     bad: 0,
   };
+  
   countTotalFeedback = () => {
     const { good, neutral, bad } = this.state;
     const total = good + neutral + bad;
